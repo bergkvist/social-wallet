88df5489378340f702384f7047025b6f7a7922a3
matsjs <matsjorgen@hotmail.com>
2018-01-12 22:16:38 +0100

    port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const
   app = express().use(bodyParser.json()); // creates express http server
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+app.listen(process.env.PORT || 8080, () => console.log('webhook is listening'));
 
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {  
