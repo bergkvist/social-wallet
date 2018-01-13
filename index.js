1beaa185bae8687dcfa87f5bdf03407d2ec9c743
matsjs <matsjorgen@hotmail.com>
2018-01-13 12:37:05 +0100

    x

--- index.js
+++ index.js
@@ -45,23 +45,7 @@ app.post('/webhook', (req, res) => {
   let body = req.body;
 
   // Check the webhook event is from a Page subscription
-  if (body.object === 'page') {
 
-    // Iterate over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-    	console.log("hei")
-
-      // Get the webhook event. entry.messaging is an array, but 
-      // will only ever contain one event, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-
-      // Get the sender PSID
-      let sender_psid = webhook_event.sender.id;
-      console.log('Sender PSID: ' + sender_psid);
-
-      
-    });
 
     // Return a '200 OK' response to all events
     res.status(200).send('EVENT_RECEIVED');
