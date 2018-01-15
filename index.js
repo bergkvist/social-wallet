04a394d954f2661e10ba72602d38d8a73afeba15
matsjs <matsjorgen@hotmail.com>
2018-01-15 13:58:01 +0100

    yo

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+const request = require('request');
 
 // Imports dependencies and set up http server
 const
@@ -111,6 +112,20 @@ function callSendAPI(sender_psid, response) {
     "message": response
   }
 }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  }); 
 
 });
 
