442f41d10f5122124b032e21bab4d6bf4030b085
matsjs <matsjorgen@hotmail.com>
2018-01-15 20:58:50 +0100

    eeee

--- index.js
+++ index.js
@@ -56,6 +56,7 @@ app.post('/webhook', (req, res) => {
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
       if (webhook_event.message) {
+        console.log(webhook_event.message.nlp)
         handleMessage(sender_psid, webhook_event.message);  
             
       } else if (webhook_event.postback) {
