a7555bd670e23a36ad176c571a3c3694f83b3591
matsjs <matsjorgen@hotmail.com>
2018-01-15 21:03:37 +0100

    eeee

--- index.js
+++ index.js
@@ -56,7 +56,9 @@ app.post('/webhook', (req, res) => {
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
       if (webhook_event.message) {
-        console.log(webhook_event.message.nlp)
+        console.log("User intent: " + webhook_event.message.nlp.intent)
+        console.log("Amount of XEM to send: " + webhook_event.message.nlp.number)
+        console.log("To contact: " + webhook_event.message.nlp.contact)
         handleMessage(sender_psid, webhook_event.message);  
             
       } else if (webhook_event.postback) {
