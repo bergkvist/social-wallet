feebba19eb6a373428d06031390decd6b93fd9a7
matsjs <matsjorgen@hotmail.com>
2018-01-28 21:51:49 +0100

    hei

--- nlp/parse.js
+++ nlp/parse.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import send from '../messenger-api-helpers/send';
 
 const determineNLP = (message, senderId) =>{
@@ -50,57 +49,4 @@ const determineIntent = (nlpInfo,senderId) => {
 export default {
 determineNLP,
 determineIntent,
-=======
-import send from '../messenger-api-helpers/send';
-
-const determineNLP = (message, senderId) =>{
-    if (message.nlp.entities){
-        const nlp = message.nlp.entities;
-        determineIntent(nlp,senderId);
-    }else {
-        send.sendHelpMessage(senderId);
-    }
-    
-
-}
-const determineIntent = (nlpInfo,senderId) => {
-    const intent = nlpInfo["intent"][0]["value"];
-    switch (intent) {
-    case 'send':
-        if (nlpInfo["number"] && nlpInfo["contact"]){
-            send.sendTransactionConfirmation(senderId, nlpInfo["number"][0]["value"], nlpInfo["contact"][0]["value"]);
-        } else {
-            //Send please include all information message.
-            send.sendMoreInfoMessage(senderId);
-        }
-
-        break;
-
-    case 'request':
-        if (nlpInfo["number"] && nlpInfo["contact"]){
-
-        } else {
-            //Send please include all information message.
-            send.sendMoreInfoMessage(senderId);
-        }
-        break;
-    case 'login':
-        send.sendWelcomeMessage(senderId);
-        break;
-
-    case 'logout':
-        send.sendSignOutPrompt(senderId);
-        break;
-        
-    default:
-    // HELP RESPONSE
-        send.sendHelpMessage(senderId);
-        break;
-    }
-}
-
-export default {
-determineNLP,
-determineIntent,
->>>>>>> 47416ab3ab4d4c4e3029ed6c6d3093343360815e
 };
